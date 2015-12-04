2608e13abb0ce6426cc5cc4ed6d2db1249de5d58
itakestime <andrew.hunkin@gmail.com>
2015-12-04 16:04:04 +1300

    Added charset

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+<meta charset="UTF-8">
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
