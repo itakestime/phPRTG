b4eecaad74e2381a51e19e5a322d2dff404c62d7
itakestime <andrew.hunkin@gmail.com>
2015-12-07 14:42:37 +1300

    Update external scripts to https

--- prtgerror.php
+++ prtgerror.php
@@ -168,7 +168,7 @@ foreach($xml->item as $item) {
 <meta charset="UTF-8">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="icon" type="img/ico" href="/favicon.ico">
 <title> PRTG Errors and warnings </title>
